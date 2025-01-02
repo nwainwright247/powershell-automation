fa27709186327afff674aa09e8743a782ab38fd9
Nick Wainwright <153974761+nwainwright247@users.noreply.github.com>
2025-01-02 15:09:55 -0500

    Add files via upload

---
+++ scripts/sh-server-.NET-Core-update.ps1
@@ -0,0 +1,2 @@
+﻿Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/6.0.26/dotnet-runtime-6.0.26-win-x64.exe" -OutFile "C:\Temp\dotnet-runtime-6.0.26-win-x64.exe"
+Start-Process -FilePath "C:\Temp\dotnet-runtime-6.0.26-win-x64.exe" -ArgumentList "/quiet" -Wait
